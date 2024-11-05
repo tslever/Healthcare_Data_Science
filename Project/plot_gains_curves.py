12b21c7a285a0dffca52011baac3b476b84132e4
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-11-05 15:05:44 -0500

    Tailored graphing gains curve and density plot

--- Project/plot_gains_curves.py
+++ Project/plot_gains_curves.py
@@ -11,38 +11,38 @@ import pandas as pd
 
 def main(name_of_model, number_of_training_or_testing_observations, path_to_data_frame):
     dictionary_of_numbers_of_standard_deviations_below_mean_and_colors = {
-        -0.125: 'red',
-        -0.25: 'green',
-        -0.375: 'blue'
+        #-0.125: 'red',
+        #-0.25: 'green',
+        -0.65: 'blue'
     }
-    data_frame_of_observed_response_values_and_average_of_predicted_response_values = pd.read_csv(path_to_data_frame)[['observed_response_value', 'average_of_predicted_response_values']]
-    series_of_observed_response_values = data_frame_of_observed_response_values_and_average_of_predicted_response_values['observed_response_value']
+    data_frame_of_observed_response_values_and_average_of_predicted_response_values = pd.read_csv(path_to_data_frame)[['actual_los', 'predicted_expected_los']]
+    series_of_observed_response_values = data_frame_of_observed_response_values_and_average_of_predicted_response_values['actual_los']
     for pair in dictionary_of_numbers_of_standard_deviations_below_mean_and_colors.items():
-        df = data_frame_of_observed_response_values_and_average_of_predicted_response_values[['observed_response_value']].copy()
+        df = data_frame_of_observed_response_values_and_average_of_predicted_response_values[['actual_los']].copy()
         z_score = pair[0]
         threshold = np.mean(series_of_observed_response_values) + z_score * np.std(series_of_observed_response_values)
         df['indicator_that_observed_response_value_is_below_threshold'] = [1 if observed_response_value < threshold else 0 for observed_response_value in series_of_observed_response_values]
-        df = df.sort_values(by = 'observed_response_value')
+        df = df.sort_values(by = 'actual_los')
         df.reset_index(drop = True, inplace = True)
-        df.drop('observed_response_value', axis = 1, inplace = True)
+        df.drop('actual_los', axis = 1, inplace = True)
         df['cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'] = np.cumsum(df['indicator_that_observed_response_value_is_below_threshold'])
         df.drop('indicator_that_observed_response_value_is_below_threshold', axis = 1, inplace = True)
         df['cumulative_relative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'] = df['cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'] / max(df['cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'])
         df.drop('cumulative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model', axis = 1, inplace = True)
         print(df.head(n = 3))
-        plt.plot(df.index, df['cumulative_relative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'], label = f'perfect model; z score = {z_score}', color = pair[1])
+        plt.plot(df.index, df['cumulative_relative_frequency_of_indicators_that_observed_response_value_is_below_threshold_for_perfect_model'], label = f'perfect model; z score = {z_score}', color = 'blue')
         df = data_frame_of_observed_response_values_and_average_of_predicted_response_values.copy()
         df['indicator_that_observed_response_value_is_below_threshold'] = [1 if observed_response_value < threshold else 0 for observed_response_value in series_of_observed_response_values]
-        df.drop('observed_response_value', axis = 1, inplace = True)
-        df.sort_values(by = 'average_of_predicted_response_values', inplace = True)
+        df.drop('actual_los', axis = 1, inplace = True)
+        df.sort_values(by = 'predicted_expected_los', inplace = True)
         df.reset_index(drop = True, inplace = True)
-        df.drop('average_of_predicted_response_values', axis = 1, inplace = True)
+        df.drop('predicted_expected_los', axis = 1, inplace = True)
         df['cumulative_frequency_of_positive_indicators_that_observed_response_value_is_below_threshold_for_trained_model'] = np.cumsum(df['indicator_that_observed_response_value_is_below_threshold'])
         df.drop('indicator_that_observed_response_value_is_below_threshold', axis = 1, inplace = True)
         df['cumulative_relative_frequency_of_positive_indicators_that_observed_response_value_is_below_threshold_for_trained_model'] = df['cumulative_frequency_of_positive_indicators_that_observed_response_value_is_below_threshold_for_trained_model'] / max(df['cumulative_frequency_of_positive_indicators_that_observed_response_value_is_below_threshold_for_trained_model'])
         df.drop('cumulative_frequency_of_positive_indicators_that_observed_response_value_is_below_threshold_for_trained_model', axis = 1, inplace = True)
         print(df.head(n = 3))
-        plt.plot(df.index, df['cumulative_relative_frequency_of_positive_indicators_that_observed_response_value_is_below_threshold_for_trained_model'], label = f'trained model; z score = {z_score}', color = pair[1])
+        plt.plot(df.index, df['cumulative_relative_frequency_of_positive_indicators_that_observed_response_value_is_below_threshold_for_trained_model'], label = f'trained model; z score = {z_score}', color = 'green')
     plt.grid()
     plt.legend()
     plt.title(f'Cumulative Relative Frequency Of Positive Indicators\nThat Observed Response Value Is Below Threshold Determined By z Score\nVs. Index In Data Frame Of {number_of_training_or_testing_observations} Indicators\nSorted By Average Of Predicted Response Values\nFor {name_of_model}', fontsize = 10)
--- Project/plot_predicted_expected_LOS_vs_observed_LOS.py
+++ Project/plot_predicted_expected_LOS_vs_observed_LOS.py
@@ -1,13 +1,14 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 
-data_frame_of_observed_and_averaged_predicted_reponse_values = pd.read_csv('actual_vs_predicted_los.csv')
+data_frame_of_observed_and_averaged_predicted_reponse_values = pd.read_csv('data_frame_of_ids_and_actual_and_predicted_expected_LOSs.csv')
 plt.scatter(
-    x = data_frame_of_observed_and_averaged_predicted_reponse_values['observed_response_value'],
-    y = data_frame_of_observed_and_averaged_predicted_reponse_values['average_of_predicted_response_values'],
+    x = data_frame_of_observed_and_averaged_predicted_reponse_values['actual_los'],
+    y = data_frame_of_observed_and_averaged_predicted_reponse_values['predicted_expected_los'],
     alpha = 0.1
 )
 plt.title('Predicted Expected LOS Vs. Observed LOS')
 plt.xlabel('Observed LOS')
 plt.ylabel('Predicted Expected LOS')
+plt.grid()
 plt.show()
